c421f00df5330bfc44e8b65872a259e2eb176f5a
Bakberchick <atymtaevbak@bk.ru>
2022-02-04 00:37:36 +0600

    added

--- src/main/java/com/atymtay/spring/course_project/entities/Course.java
+++ src/main/java/com/atymtay/spring/course_project/entities/Course.java
@@ -3,16 +3,13 @@ package com.atymtay.spring.course_project.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 import javax.persistence.*;
-import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @MappedSuperclass
-//@Table(name="course")
 abstract class Course {
 
     @Id
